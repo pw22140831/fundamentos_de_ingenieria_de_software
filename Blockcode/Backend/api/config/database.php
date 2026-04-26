e798d52bec96ee5be168bedf090123ee3d0df563
RSNezuko <l22140812@queretaro.tecnm.mx>
2026-04-26 16:12:50 -0600

    Agregado .gitignore y uso de .env
    Se agrego el archivo .gitignore de manera global y se agrego el uso de .env para DB

--- Blockcode/Backend/api/config/database.php
+++ Blockcode/Backend/api/config/database.php
@@ -1,9 +1,15 @@
 <?php
-$host = "localhost";
-$port = "5432";
-$dbname ="my_app";
-$user = "my_app_role";
-$password = "some_password";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Cargar variables de entorno desde Backend/api/.env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$host     = $_ENV['DB_HOST'];
+$port     = $_ENV['DB_PORT'];
+$dbname   = $_ENV['DB_NAME'];
+$user     = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
