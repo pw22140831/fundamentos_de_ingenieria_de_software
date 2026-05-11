ecdecd7966a3b1147d75294cda56d416bf1ee4a6
yxsua <yx.suaa@gmail.com>
2026-05-11 06:31:56 +0000

    JWT Fixes

--- Blockcode/Backend/api/v1/auth/login.php
+++ Blockcode/Backend/api/v1/auth/login.php
@@ -5,6 +5,10 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
 
 include("../../config/database.php");
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../');
+$dotenv->load();
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
